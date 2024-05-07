d085e76c7c636dc8136251b6748a5ac4bd8addef
Gauri <gaurijindal.hrd@gmail.com>
2024-05-07 23:13:28 +0530

    Add project files.

---
+++ FormValidation/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using FormValidation.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FormValidation.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult GetAccount()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult PostAccount(AccountModel account)
+        {
+            if(ModelState.IsValid)
+            {
+                return View("success");
+            }
+            return RedirectToAction("GetAccount");
+        }
+    }
+}
---
+++ FormValidation/Models/AccountModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FormValidation.Models
+{
+    public class AccountModel
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(8)]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(20)]
+        public string ? Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string ? Email { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string ? Password { get; set; }
+
+        [Required]
+        public string ? Website { get; set; }
+
+        [Required]
+        [Range(12, 50)]
+        public string ? Age { get; set; }
+    }
+}
